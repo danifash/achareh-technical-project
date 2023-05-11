f2e476a1f8fad2a312a8ed568cac6a522ba94c3a
Danial <d.bayati28@gmail.com>
2023-05-11 09:14:14 +0330

    feat(layout): create DefaultLayout and config routers

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="Layout">
+    <!-------- Header -------->
+    <BaseHeader />
+    <!-------- Main content -------->
+    <main>
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import BaseHeader from "./modules/BaseHeader.vue";
+</script>
